c96313f6499296f7672ae6f9a422fc3c58c2734e
tun19502 <johnlawall24@gmail.com>
2024-10-16 15:55:00 -0400

    commit finished

--- app/src/main/java/edu/temple/fragmentrefactor/BlankFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/BlankFragment.kt
@@ -9,16 +9,11 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class BlankFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -26,7 +21,7 @@ class BlankFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+        // Inflate
         return inflater.inflate(R.layout.fragment_blank, container, false).apply{
 
             val displayTextView = findViewById<TextView>(R.id.displayTextView)
